47f02560e422bb7c1490b6b717ee308310f59006
Ruslan Ruslan <kubsonr1@gmail.com>
2024-03-21 21:21:16 +0500

    created classes for map, market, menu

---
+++ MarkerPopupMenu.py
@@ -0,0 +1,11 @@
+from kivymd.uix.bottomsheet import MDListBottomSheet
+class MarkerPopupMenu(MDListBottomSheet):
+    def __init__(self, parkData):
+        super().__init__()
+        headers = "1,2,3"
+        headers = headers.split(',')
+
+        for i in range(len(headers)):
+            attributeName = headers[i]
+            attributeValue = parkData
+            setattr(self,attributeName,attributeValue)
